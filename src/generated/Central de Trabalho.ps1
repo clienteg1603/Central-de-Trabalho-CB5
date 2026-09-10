0f84ed937e3ebbfedd6913c1138f8eb82357e055
Central de Trabalho Builder <actions@users.noreply.github.com>
2026-09-10 17:42:31 +0000

    Publica Central de Trabalho v0.16.5 no canal Teste

--- src/generated/Central de Trabalho.ps1
+++ src/generated/Central de Trabalho.ps1
@@ -33,7 +33,7 @@ function Set-CentralTitleBarTheme {
     } catch {}
 }
 
-$script:AppVersion = "0.16.4"
+$script:AppVersion = "0.16.5"
 $script:RootPath = $PSScriptRoot
 $script:GeneratorVersion = "3.7.2"
 $script:MaintenanceVersion = "0.6.1"
